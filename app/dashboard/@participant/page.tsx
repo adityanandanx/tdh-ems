0d071e2749ab8814c4d94386af3c275d404c94be
iMADi-ARCH <nandanaditya985@gmail.com>
2024-02-08 00:30:27 +0530

    feat: coming soon page

--- app/dashboard/@participant/page.tsx
+++ app/dashboard/@participant/page.tsx
@@ -1,9 +1,15 @@
+import { TimerIcon } from "lucide-react";
 import React from "react";
 
 type Props = {};
 
 const ParticipantDashboard = (props: Props) => {
-  return <div>ParticipantDashboard</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-[80vh] ">
+      <h1 className="text-3xl">Coming Soon...</h1>
+      <p>Stay tuned 😉</p>
+    </div>
+  );
 };
 
 export default ParticipantDashboard;
