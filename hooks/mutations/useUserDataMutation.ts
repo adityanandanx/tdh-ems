47258cb6ddb63d264e829d264dc353ca4e5853f9
iMADi-ARCH <nandanaditya985@gmail.com>
2024-02-16 18:17:02 +0530

    refactor: automatically get user id when mutating user data

--- hooks/mutations/useUserDataMutation.ts
+++ hooks/mutations/useUserDataMutation.ts
@@ -2,18 +2,26 @@ import { useToast } from "@/components/ui/use-toast";
 import { useSupabase } from "@/lib/supabase/client";
 import { UsersRow } from "@/lib/supabase/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { count } from "console";
+import { useUserQuery } from "../queries";
 
 export default function useUserDataMutation() {
   const supabase = useSupabase();
+  const { data: user } = useUserQuery();
   const qc = useQueryClient();
   const { toast } = useToast();
   return useMutation({
-    mutationFn: async (newValues: Partial<UsersRow> & { id: string }) => {
+    mutationFn: async (newValues: Partial<UsersRow>) => {
+      if (!user?.id) {
+        toast({
+          title: "You are not logged in.",
+          description: "Login first to change data.",
+        });
+        return null;
+      }
       const { data, error, count } = await supabase
         .from("users")
         .update(newValues)
-        .eq("id", newValues.id);
+        .eq("id", user.id);
       if (error) throw error;
       return count;
     },
